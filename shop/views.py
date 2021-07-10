2aefcc544c08324cc8b98a38ad337a4b6cc77fe7
Julio <julio@ctrlsun.com>
2021-07-10 15:28:38 -0500

    Added models, views and wiring to urls.

--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
-# Create your views here.
+from .models import Category, Product
+
+
+def product_list(request, category_slug=None):
+    """List available products."""
+
+    category = None
+    categories = Category.objects.all().order_by("-name")
+    products = Product.objects.filter(available=True).order_by("-name")
+
+    if category_slug:
+        category = get_object_or_404(Category, slug=category_slug)
+        products = products.filter(category=category)
+
+    context = {
+        "category": category,
+        "categories": categories,
+        "products": products
+    }
+    return render(request, "shop/products/list.html", context)
+
+
+def product_detail(request, id, slug):
+    """List a single product."""
+
+    product = get_object_or_404(Product, id=id, slug=slug, available=True)
+    context = {"product": product}
+
+    return render(request, "shop/products/detail.html", context)
